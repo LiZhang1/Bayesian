64ce8974f213c07bba4ee73b6e1c4adb93777e36
LiZhang1 <kxycindy@gmail.com>
2014-09-16 18:01:36 -0600

    initial

---
+++ Bios_7717_hw1.R
@@ -0,0 +1,13 @@
+##################################################
+# Task: Bios 7717: Bayesian Biostatistics, HW 1 ##
+# Author: Li Zhang                              ##
+# Date created: 9/16/2014                       ##
+# Date modified:                                ##
+##################################################
+
+library (VGAM)
+library (binom)
+## Problem 1, Lyme test, specificity
+# data
+y <- 19
+n <- 25
